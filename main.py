a38f7e538a5cf7bd4791da48ec76c926ad8fe116
Artem Larionenko <74620760+lrrrtm@users.noreply.github.com>
2021-11-28 01:00:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,16 +24,10 @@ temporaryDict = {}
 
 def resizePicture(current):
     image_path = current
-    # указываем фиксированный размер стороны
     fixed_width = 500
     img = Image.open(image_path)
-    # получаем процентное соотношение
-    # старой и новой ширины
     width_percent = (fixed_width / float(img.size[0]))
-    # на основе предыдущего значения
-    # вычисляем новую высоту
     height_size = int((float(img.size[0]) * float(width_percent)))
-    # меняем размер на полученные значения
     new_image = img.resize((fixed_width, height_size))
     new_image.save(current)
 
